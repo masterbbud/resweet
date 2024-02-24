709312db125eeb36b1998ab54de645471076fbf8
Brandon Faunce <masterbbud@gmail.com>
2024-02-24 15:54:56 -0500

    Added libraries

--- app/flutter_app/lib/interfaces.dart
+++ app/flutter_app/lib/interfaces.dart
@@ -9,6 +9,24 @@ class Receipt {
   final List<RItem> items;
 
   Receipt({required this.name, required this.date, required this.assignee, required this.items});
+
+  factory Receipt.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'name': String name,
+        'date': String date,
+        'assignee': Map<String, dynamic> assignee,
+        'items': List<Map<String, dynamic>> items
+      } =>
+        Receipt(
+          name: name,
+          date: date,
+          assignee: User.fromJson(assignee),
+          items: items.map((i) => RItem.fromJson(i)).toList()
+        ),
+      _ => throw const FormatException('Failed to load receipt.'),
+    };
+  }
 }
 
 class User {
@@ -17,6 +35,22 @@ class User {
   final Color color;
 
   User({required this.uuid, required this.name, required this.color});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'uuid': String uuid,
+        'name': String name,
+        'groupIndex': int groupIndex // TODO FIX
+      } =>
+        User(
+          uuid: uuid,
+          name: name,
+          color: getColor(groupIndex)
+        ),
+      _ => throw const FormatException('Failed to load user.'),
+    };
+  }
 
   String getInitials() {
     return name.split(' ').map((e) => e[0].toUpperCase()).toList().join("");
@@ -29,6 +63,22 @@ class RItem {
   final List<User> payers;
 
   RItem({required this.name, required this.price, required this.payers});
+
+  factory RItem.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'name': String name,
+        'price': double price,
+        'payers': List<Map<String, dynamic>> payers
+      } =>
+        RItem(
+          name: name,
+          price: price,
+          payers: payers.map((i) => User.fromJson(i)).toList()
+        ),
+      _ => throw const FormatException('Failed to load RItem.'),
+    };
+  }
 }
 
 class Group {
@@ -36,4 +86,41 @@ class Group {
   final String name;
 
   Group({required this.members, required this.name});
+
+  factory Group.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'name': String name,
+        'members': List<Map<String, dynamic>> members
+      } =>
+        Group(
+          name: name,
+          members: members.map((i) => User.fromJson(i)).toList()
+        ),
+      _ => throw const FormatException('Failed to load Group.'),
+    };
+  }
+}
+
+Color getColor(int groupIndex) {
+  switch (groupIndex) {
+    case 0:
+      return Colors.red;
+    case 1:
+      return Colors.orange;
+    case 2:
+      return Colors.yellow;
+    case 3:
+      return Colors.green;
+    case 4:
+      return Colors.blue;
+    case 5:
+      return Colors.purple;
+    case 6:
+      return Colors.brown;
+    case 7:
+      return Colors.deepPurple;
+    default:
+      return Colors.grey;
+  }
 }
