7c2148039c7ecaf343ab73b874a7c5b06071266d
Brandon Faunce <masterbbud@gmail.com>
2024-02-25 02:15:20 -0500

    Finished receipt verification flow

--- app/flutter_app/lib/screens/ProfilePage.dart
+++ app/flutter_app/lib/screens/ProfilePage.dart
@@ -19,7 +19,7 @@ class ProfilePageState extends State<ProfilePage> {
   @override
   void initState() {
     super.initState();
-    // myUser = info.myUser;
+    myUser = info.myUser;
   }
 
   @override
---
+++ app/flutter_app/lib/screens/ReceiptConfirmPage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/interfaces.dart';
+import 'package:flutter_app/managers/ApiManager.dart';
+import 'package:flutter_app/managers/EverythingManager.dart';
+import 'package:flutter_app/screens/NavBar.dart';
+import 'package:flutter_app/screens/ReceiptAssignmentPage.dart';
+
+class ReceiptConfirmPage extends StatelessWidget {
+  ReceiptConfirmPage({Key? key, required this.receipt}) : super(key: key);
+  Receipt receipt;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final int test = 0;
+
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(80.0),
+          child: Center(
+            child: Column(
+              children: [
+                Spacer(),
+                Text("Your receipt has been saved!"),
+                Spacer(),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: info.allUsers.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Row(children: [
+                        Text(info.allUsers[index].name),
+                        Spacer(),
+                        Text('\$${receipt.getPriceForUser(info.allUsers[index]).toStringAsFixed(2)}')
+                      ],);
+                    }),
+                ),
+                //SizedBox(height: 10),
+                Row(children: [
+                  Text("Paid for by ${receipt.assignee.name}"),
+                  Spacer(),
+                  Text("\$${receipt.getTotal().toStringAsFixed(2)}")
+                ],),
+                
+                Spacer(),
+                TextButton(onPressed: () {
+                  Navigator.pop(context);
+                }, child: Text("Go Home"))
+              ],
+            )
+          ),
+      ),
+    );
+  }
+}
