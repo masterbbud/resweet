5d29ac62ab6370b49e8d56e8f3b7058b241e86d4
thatnoobles <danildonchuk@gmail.com>
2024-02-25 10:43:26 -0500

    get_user takes token

--- server/api/users.py
+++ server/api/users.py
@@ -1,5 +1,6 @@
 from typing import Any, Dict
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
+from db.auth import *
 from db.users import *
 from .mapper import *
 import api.models as api
@@ -16,8 +17,8 @@ async def register_user(req: api.UserPost):
     return to_api_user(add_user(user))
 
 @app.get("")
-async def get_user(id: str):
-    user = get_user_by_uuid(id)
+async def get_user(req: Request):
+    user = authenticate(req.headers["token"])
 
     if user is None:
         raise HTTPException(404, f"User {id} not found")
