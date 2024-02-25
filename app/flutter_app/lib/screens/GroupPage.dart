439b54927369387c790b165fb5f0570e66e2c994
jl7739 <109109212+snower7@users.noreply.github.com>
2024-02-25 08:01:49 -0500

    GROUP PAGE ADD ROOMMATES BUTTON

--- app/flutter_app/lib/screens/GroupPage.dart
+++ app/flutter_app/lib/screens/GroupPage.dart
@@ -13,6 +13,7 @@ class GroupPage extends StatefulWidget {
 
 class GroupPageState extends State<GroupPage> {
   List<User> users = [];
+  TextEditingController usernameController = TextEditingController(text: '');
 
   @override
   void initState() {
@@ -21,28 +22,107 @@ class GroupPageState extends State<GroupPage> {
     print(users);
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.fromLTRB(15, 30, 0, 30),
-            child: Text("Your group",
+    return Column(children: [
+      Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.fromLTRB(15, 30, 0, 30),
+          child: Text("Your group",
+              style: TextStyle(
+                  fontSize: 44,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontFamily: 'BagelFatOne'))),
+      Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        child: SizedBox(
+          height: 40,
+          child: TextButton(
+            onPressed: () {
+              showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                        padding: const EdgeInsets.fromLTRB(50, 20, 50, 15),
+                        child: Column(
+                          children: [
+                            Container(
+                                alignment: Alignment.topLeft,
+                                padding:
+                                    const EdgeInsets.fromLTRB(0, 30, 0, 30),
+                                child: Text("Enter username",
+                                    style: TextStyle(
+                                        fontSize: 44,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary,
+                                        fontFamily: 'BagelFatOne'))),
+                            SizedBox(
+                                height: 100,
+                                // width: MediaQuery.of(context).size.width - 100,
+                                child: TextField(
+                                  controller: usernameController,
+                                  clipBehavior: Clip.none,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: '@username',
+                                  ),
+                                )),
+                            Row(children: [
+                              const Spacer(),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text("Add",
+                                    style: TextStyle(
+                                        fontFamily: 'Inter',
+                                        fontSize: 24,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .background)),
+                                style: ButtonStyle(
+                                    padding: MaterialStateProperty.all<EdgeInsets>(
+                                        const EdgeInsets.fromLTRB(20, 15, 20, 15)),
+                                    backgroundColor: MaterialStateProperty.all<Color>(
+                                        Theme.of(context).colorScheme.onPrimary),
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(5.0),
+                                            side: BorderSide(
+                                                color: Theme.of(context).colorScheme.onPrimary)))),
+                              )
+                            ])
+                          ],
+                        ));
+                  });
+            },
+            child: Text("Add roommates",
                 style: TextStyle(
-                    fontSize: 44,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    fontFamily: 'BagelFatOne'))),
-        Container(
+                    fontFamily: 'Inter',
+                    fontSize: 24,
+                    color: Theme.of(context).colorScheme.background)),
+            style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                    const EdgeInsets.fromLTRB(15, 10, 15, 10)),
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).colorScheme.onPrimary),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(
+                            color: Theme.of(context).colorScheme.onPrimary)))),
+          ),
+        ),
+      ),
+      Container(
           padding: const EdgeInsets.symmetric(horizontal: 40.0),
           child: Column(
             children: users.map((i) {
               return UserItem(user: i);
             }).toList(),
-          )
-        )]
-    );
+          ))
+    ]);
   }
 }
 
